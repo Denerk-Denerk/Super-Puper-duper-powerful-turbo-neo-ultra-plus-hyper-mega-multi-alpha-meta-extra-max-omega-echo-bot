a06ba5c1aa14fc4e992457072685ee5f014f32a3
ツDenerk <den.zyr00@gmail.com>
2025-08-25 23:40:41 +0400

    update 4.0

--- hello.py
+++ hello.py
@@ -6,42 +6,45 @@ import os
 from aiogram.filters import Command, CommandObject
 from aiogram import Bot, Dispatcher, types
 from dotenv import load_dotenv
+from datetime import datetime
 
 
 load_dotenv()
 
 
-def write_to_csv_file(name, text):
+def write_to_csv_file(name, text, user_name, user_id, date):
     with open(name, 'a', encoding='utf8', newline='') as f:
         writer = csv.writer(f, delimiter=';')
-        writer.writerow([text])
+        writer.writerow([user_name, date, text, user_id])
 
         
-def csv_file_reader(name_csv):
-    with open(name_csv, newline='', encoding='utf-8') as f:
+def reader_to_csv_file(name_csv):
+    with open(name_csv, newline='', encoding='utf-8', mode='r') as f:
         reader = csv.reader(f, delimiter=';')
-        return [row for row in reader]
+        return [row for row in reader][::-1]
 
+
+def create_format(row):
+    return f"{row[0]} | {row[1]} | {row[2]}"
 
 bot = Bot(token=os.getenv("TOKEN"), encode='utf-8')
 dp = Dispatcher()
 
 @dp.message(Command('history'))
 async def history(message: types.Message, command: CommandObject):
-    history = csv_file_reader('file.csv')
+    history = reader_to_csv_file('file.csv')
     length = command.args
     if length:
-        await message.answer('\n'.join([row[0] for row in history[:int(length)] if row]))
+        await message.answer('\n'.join([create_format(row) for row in history[:int(length)] if row]))
     else:
-        await message.answer('\n'.join([row[0] for row in history if row]))
+        await message.answer('\n'.join([create_format(row) for row in history[:10] if row]))
 
 
 
 @dp.message()
 async def echo(message: types.Message):
     await message.answer(message.text)
-    write_to_csv_file('file.csv', message.text)
-    
+    write_to_csv_file('file.csv', message.text, message.from_user.first_name, message.from_user.id, datetime.strptime(str(message.date.astimezone()), "%Y-%m-%d %H:%M:%S%z").strftime("%Y-%m-%d %H:%M:%S"))
 
 async def start():
     await dp.start_polling(bot)
